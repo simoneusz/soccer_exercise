beb0bd70e0dd99fd60617dd076401c71d5f814ec
Ilya <simonewarlet@gmail.com>
2024-11-22 12:03:09 +0200

    Add table formatting

--- lib/table.rb
+++ lib/table.rb
@@ -2,7 +2,25 @@
 
 # Table class create a table of tournament
 class Table
+  attr_accessor :matches, :table
+
   def initialize
-    @table = {}
+    @table = Hash.new(0)
+    @matches = []
+  end
+
+  def add_matches_to_table
+    @matches.each do |match|
+      points = match.points
+      table[match.team_x] += points[0]
+      table[match.team_y] += points[1]
+    end
+  end
+
+  def format_scores
+    sorted_scores = @table.sort_by { |team, points| [-points, team] }
+    sorted_scores.map.with_index(1) do |(team, points), index|
+      "#{index}. #{team}, #{points} pt#{'s' if points != 1}"
+    end.join("\n")
   end
 end
