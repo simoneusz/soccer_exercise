fd95a20bcb15a6122714dd74a4e69647828f95b9
Ilya <simonewarlet@gmail.com>
2024-11-22 12:02:27 +0200

    Add read_tale_from_file method

--- lib/table_reader.rb
+++ lib/table_reader.rb
@@ -3,5 +3,10 @@
 # TableReader class that reads data from file as a tournament table
 class TableReader
   def read_table_from_file(path)
+    file = File.new path
+    content = file.read
+
+    file.close
+    content
   end
 end
