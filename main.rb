3b1e0955399be77074736f7aa22afce5be85cb84
Ilya <simonewarlet@gmail.com>
2024-11-22 12:04:27 +0200

    Add app starting point and reading path from ARGS

--- main.rb
+++ main.rb
@@ -2,8 +2,19 @@
 
 require_relative('lib/table')
 require_relative('lib/table_reader')
+require_relative('lib/match')
 
-def main(*args)
+def main(file_path = ARGV[0])
+  reader = TableReader.new
+  table_raw = reader.read_table_from_file(file_path)
+
+  table = Table.new
+  table_raw.split("\n").each do |i|
+    match = Match.new(i)
+    table.matches << match
+  end
+  table.add_matches_to_table
+  puts table.format_scores
 end
 
-main(ARGV)
+main
